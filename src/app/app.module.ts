05e2599a8023e9b50508cff544285b2101c82e7f
Álvaro Zájara <alzca1@gmail.com>
2020-03-01 18:09:54 +0100

    added cart and its functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { BackHomebtnComponent } from './back-homebtn/back-homebtn.component';
+import {CartComponent} from './cart/cart.component'; 
 
 @NgModule({
   imports: [
@@ -16,7 +17,8 @@ import { BackHomebtnComponent } from './back-homebtn/back-homebtn.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent }
+      { path: 'products/:productId', component: ProductDetailsComponent }, 
+      {path: 'cart', component: CartComponent}
     ])
   ],
   declarations: [
@@ -25,7 +27,8 @@ import { BackHomebtnComponent } from './back-homebtn/back-homebtn.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    BackHomebtnComponent
+    BackHomebtnComponent, 
+    CartComponent
   ],
   bootstrap: [ AppComponent ]
 })
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import {products}  from '../products';
-import {ActivatedRoute} from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { products } from "../products";
+import { CartService } from '../cart.service';
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
 })
 export class ProductDetailsComponent implements OnInit {
-product; 
-
-  constructor(
-    private route : ActivatedRoute,
-  ) { }
-
-  ngOnInit() {
-    //parte liosa de comprender. copiar y pegar. punto!
-    this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')]
-    })
+  product;
+  addToCart(product:any){
+    this.cartservice.addToCart(product);
+    window.alert('Product added!');
   }
 
+  constructor(
+    private route: ActivatedRoute, 
+    private cartservice : CartService
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get("productId")];
+    });
+  }
 }
